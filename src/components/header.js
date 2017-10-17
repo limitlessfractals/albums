59b776ed7472b7f68c0c38ee5867c34c4ef2d1bd
limitlessfractals <ikkyu@MacBook-Pro.lan>
2017-10-17 15:58:47 -0500

    header

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Create a component
+const Header = () => {
+    return <Text>Albums</Text>;
+};
+	
+// Make the component available to othre parts of the app
+export default Header;
