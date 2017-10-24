1b45a50a3b42f2f4beba0b6ac7b79a3b221d520a
limitlessfractals <fanwuq@gmail.com>
2017-10-24 11:29:48 -0500

    component level state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,11 +3,15 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+   //init empty array as component level state
+   state = { albums: [] };
+   // use setState rather than = to modify state
    componentWillMount() {
-      axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+      axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data }));
    }
 
    render() {
+      console.log(this.state);
       return (
          <View>
             <Text>Album List!!</Text>
