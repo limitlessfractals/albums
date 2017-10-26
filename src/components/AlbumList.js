abf3b9f5a8b088de8e201fbfbceef19fa880c52b
limitlessfractals <fanwuq@gmail.com>
2017-10-26 12:34:09 -0500

    AlbumDetail.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
    // init empty array as component level state
@@ -12,7 +13,8 @@ class AlbumList extends Component {
    }
 
    renderAlbums() {
-      return this.state.albums.map(album => <Text>{album.title}</Text>);
+      // ideally key should be ID
+      return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
    }
 
    render() {
